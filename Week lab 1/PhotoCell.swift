b1e07c608f4d00cb0840c4300722593b85f26930
Softmatech <ingandy75@gmail.com>
2018-09-14 16:55:20 -0400

    Inicial Commit

---
+++ Week lab 1/PhotoCell.swift
@@ -0,0 +1,25 @@
+//
+//  TableViewCell.swift
+//  Week lab 1
+//
+//  Created by Joseph Andy Feidje on 9/14/18.
+//  Copyright © 2018 Softmatech. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageCellView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Week lab 1/PhotosViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PhotosViewController.swift
+//  Week lab 1
+//
+//  Created by Joseph Andy Feidje on 9/14/18.
+//  Copyright © 2018 Softmatech. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
